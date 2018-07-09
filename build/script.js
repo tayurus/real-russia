cef3c3d29f69340096fde88d18a3fda908cddcff
Yury Tatarintsev <yury@realrussialimited.local>
2018-07-09 09:33:52 +0300

    add input mask to datepickers

--- build/script.js
+++ build/script.js
@@ -536,7 +536,13 @@ $( ".datepicker_jq").datepicker({
       changeMonth: true,
       changeYear: true,
       // yearRange: (typeof($(this).attr('data-minyear')) === "undefined") ? minDefaultYear + ":" + currentYear : $(this).attr('data-minyear') + ":" + currentYear
-});
+}).mask('99/99/9999');
+
+$( ".datepicker_jq").change(function(){
+    if ($(this).datepicker('getDate').toDateString() === new Date().toDateString())
+        $(this).datepicker("setDate", new Date());
+        // $(this).datepicker("setDate", new Date(d.getFullYear()+1,d.getMonth(),d.getDate()));
+})
 
 $('.hint').click(function(event) {
     event.stopPropagation()
--- build/tourist-visa-final.asp
+++ build/tourist-visa-final.asp
@@ -4,6 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.js"></script>
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>
 <link rel="stylesheet" href="styles.css"/>
 <script src="script.js" defer="defer"></script>
